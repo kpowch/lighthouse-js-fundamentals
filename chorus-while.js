c6a358c99d4ba2e0d1e9d99935be57fb8a9464cd
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-02-21 01:05:12 +0000

    added new exercises

---
+++ chorus-while.js
@@ -0,0 +1,20 @@
+var chorus = "Let's dance!";
+
+// while loop implementation
+/*
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+*/
+
+// for loop implementation
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
